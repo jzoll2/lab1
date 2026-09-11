7fabd305d7a9eddf439faef4c14fbc0dc5d4934a
Your Name <jzoll@umass.edu>
2026-09-11 16:39:00 +0000

    initial Express server with two routes

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+const projects = [
+  { name: 'Weather app', tag: 'javascript' },
+  { name: 'Portfolio site', tag: 'express' },
+  { name: 'Budget tracker', tag: 'python' },
+];
+
+app.get("/about-me", (req, res) => {
+  res.send("About me");
+});
+
+app.get("/info", (req, res) => {
+  res.send("I am a CS major!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Home!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, web!");
+});
+
+app.get('/projects', (req, res) => {
+  const tag = req.query.tag || 'fallback';
+
+  const filteredProjects = tag === 'fallback'
+    ? projects
+    : projects.filter(project => project.tag === tag);
+
+  res.send(filteredProjects);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
